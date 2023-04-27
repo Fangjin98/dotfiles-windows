ab7aca423dc02eb5510488f50684a723a9c01c3f
Joey <fangjin98@mail.ustc.edu.cn>
2023-04-27 15:15:01 +0800

    init

---
+++ Install.ps1
@@ -0,0 +1,20 @@
+$global:BASE_DIR = git rev-parse --show-toplevel
+$global:CONFIG_DIR = $GIT_DIR+"/configs"
+
+Write-Host "Setting powershell to allow execution of scripts..."
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser
+
+Write-Host "Installing PSReadLine..."
+Install-Module -Name PSReadLine  -Scope CurrentUser
+
+Write-Host "Installing Scoop..."
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser 
+irm get.scoop.sh | iex
+
+Write-Host "Installing CLI apps..."
+scoop install vim sudo starship lsd zoxide gh
+scoop bucket add extras
+scoop install komorebi whkd
+
+Write-Host "Syncing configs..."
+./Install-Dotfiles.ps1
