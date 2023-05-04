1b57e49e4d30137e07e15c73f0ec2111e955599d
Joey <fangjin98@mail.ustc.edu.cn>
2023-05-04 10:32:21 +0800

    add starship config

--- Install-Dotfiles.ps1
+++ Install-Dotfiles.ps1
@@ -5,3 +5,4 @@ New-Item -ItemType SymbolicLink -Path "$HOME/Documents/PowerShell/Microsoft.Powe
 New-Item -ItemType SymbolicLink -Path "$HOME/.config/whkdrc" -Value "$BASE_DIR/dotfiles/.config/whkdrc"
 New-Item -ItemType SymbolicLink -Path "$HOME/.config/yasb/" -Value "$BASE_DIR/dotfiles/.config/yasb/"
 New-Item -ItemType SymbolicLink -Path "$HOME/.config/komorebi/" -Value "$BASE_DIR/dotfiles/.config/komorebi/"
+New-Item -ItemType SymbolicLink -Path "$HOME/.config/starship.toml" -Value "$BASE_DIR/dotfiles/.config/starship.toml/"
